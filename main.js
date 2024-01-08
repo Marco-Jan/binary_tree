52d8d3af08924dc639671e9fa28dcdb976c7a324
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-01-08 12:34:01 +0100

    add some Methode

--- main.js
+++ main.js
@@ -22,6 +22,111 @@ class Tree {
         node.right = this.buildTree(myData, mid + 1, end);
         return (this.root = node);
     }
+    //Iterativly integration of insert Methode
+    insert(val) {
+        const newNode = new TreeNode(val);
+        if (!this.root)
+            return (this.root = newNode);
+        let current = this.root;
+        while (true) {
+            // if( val === current.value) return; oder ...
+            if (val === current.value) {
+                if (!current.left) {
+                    return (current.left = newNode);
+                }
+                current = current.left;
+            }
+            if (val < current.value) {
+                if (!current.left) {
+                    return (current.left = newNode);
+                }
+                current = current.left;
+            }
+            if (val > current.value) {
+                if (!current.right) {
+                    return (current.right = newNode);
+                }
+                current = current.right;
+            }
+        }
+    }
+    //recursiv insert method
+    recursivMethod(val) {
+        var _a;
+        const current = (node) => {
+            var _a, _b;
+            if (node.value === val)
+                return;
+            if (node.value > val) {
+                current((node.left = (_a = node.left) !== null && _a !== void 0 ? _a : new TreeNode(val)));
+            }
+            if (node.value < val) { // Nullischer Koaleszenzoperator (??)
+                current((node.right = (_b = node.right) !== null && _b !== void 0 ? _b : new TreeNode(val)));
+            }
+        };
+        current((this.root = (_a = this.root) !== null && _a !== void 0 ? _a : new TreeNode(val)));
+    }
+    //find Method
+    find(val) {
+        var _a;
+        const search = (node, value) => {
+            var _a;
+            if (node === null)
+                return null;
+            if (node.value === value)
+                return node;
+            return (_a = search(node.left, value)) !== null && _a !== void 0 ? _a : search(node.right, value);
+        };
+        return (_a = search(this.root, val)) !== null && _a !== void 0 ? _a : -1;
+    }
+    // breadth search (breitensuche)
+    breadthFirst() {
+        if (!this.root)
+            return -1;
+        let node = this.root;
+        const queue = [];
+        const visited = [];
+        while (queue.length) {
+            node = queue.shift();
+            visited.push(node.value);
+            if (node.left)
+                queue.push(node.left);
+            if (node.right)
+                queue.push(node.right);
+        }
+        return visited;
+    }
+    //depth first search
+    // Pre-Order DFS
+    preOrder(node = this.root) {
+        let values = [];
+        if (node) {
+            values.push(node.value);
+            values = values.concat(this.preOrder(node.left));
+            values = values.concat(this.preOrder(node.right));
+        }
+        return values;
+    }
+    //inOrder search
+    inOrder(node = this.root) {
+        let values = [];
+        if (node) {
+            values = values.concat(this.inOrder(node.left));
+            values.push(node.value);
+            values = values.concat(this.inOrder(node.right));
+        }
+        return values;
+    }
+    // Post-Order DFS
+    postOrder(node = this.root) {
+        let values = [];
+        if (node) {
+            values = values.concat(this.postOrder(node.left));
+            values = values.concat(this.postOrder(node.right));
+            values.push(node.value);
+        }
+        return values;
+    }
 }
 function prettyPrint(node, prefix = "", isLeft = true) {
     if (node.right !== null) {
@@ -35,5 +140,20 @@ function prettyPrint(node, prefix = "", isLeft = true) {
 const data = [5, 10, 21, 87, 301, 350];
 const tree = new Tree();
 tree.buildTree(data);
-//console.log(tree);
-prettyPrint(tree.root);
+// console.log(tree);
+// tree.insert(12);
+// tree.insert(10);
+// tree.insert(2);
+// tree.insert(9);
+// tree.insert(1);
+// tree.insert(5);
+// tree.insert(11);
+// tree.insert(6);
+// tree.insert(8);
+//tree.recursivMethod(12);
+console.log("breadthfirst: ", tree.breadthFirst());
+console.log("preorder: ", tree.preOrder());
+console.log("inorder: ", tree.inOrder());
+console.log("postorder: ", tree.postOrder());
+//prettyPrint(tree.root as TreeNode<number>)
+//# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -22,17 +22,123 @@ class Tree<T> {
         node.right = this.buildTree(myData, mid + 1, end);
         return (this.root = node);
     }
+    //Iterativly integration of insert Methode
+    insert(val: T) {
+        const newNode = new TreeNode<T>(val);
+        if (!this.root) return (this.root = newNode);
+        let current = this.root;
+        while (true) {
+            // if( val === current.value) return; oder ...
+            if (val === current.value) {
+                if (!current.left) {
+                    return (current.left = newNode);
+                }
+                current = current.left;
+            }
+            if (val < current.value) {
+                if (!current.left) {
+                    return (current.left = newNode);
+                }
+                current = current.left;
+            }
+            if (val > current.value) {
+                if (!current.right) {
+                    return (current.right = newNode);
+                }
+                current = current.right;
+            }
+        }
+    }
+
+    //recursiv insert method
+
+    recursivMethod(val: T) {
+        const current = (node: TreeNode<T>) => {
+            if (node.value === val) return;
+            if (node.value > val) {
+                current((node.left = node.left ?? new TreeNode<T>(val)))
+            }
+            if (node.value < val) {  // Nullischer Koaleszenzoperator (??)
+                current((node.right = node.right ?? new TreeNode<T>(val)))
+            }
+        };
+        current((this.root = this.root ?? new TreeNode<T>(val)));
+    }
+
+
+    //find Method
+    find(val: T) {
+        const search: any = (node: TreeNode<T>, value: number) => {
+            if (node === null) return null;
+            if (node.value === value) return node;
+            return search(node.left!, value) ?? search(node.right!, value);
+        };
+        return search(this.root!, val) ?? -1;
+    }
+    // breadth search (breitensuche)
+    breadthFirst() {
+        if (!this.root) return -1;
+        let node = this.root;
+
+        const queue: TreeNode<T>[] = [];
+        const visited: T[] = [];
+
+        while (queue.length) {
+            node = queue.shift() as TreeNode<T>;
+            visited.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return visited;
+    }
+
+    //depth first search
+    // Pre-Order DFS
+    preOrder(node: TreeNode<T> | null = this.root): T[] {
+        let values: T[] = [];
+        if (node) {
+            values.push(node.value);
+            values = values.concat(this.preOrder(node.left));
+            values = values.concat(this.preOrder(node.right));
+        }
+        return values;
+    }
+    //inOrder search
+    inOrder(node: TreeNode<T> | null = this.root): T[] {
+        let values: T[] = [];
+        if (node) {
+            values = values.concat(this.inOrder(node.left));
+            values.push(node.value);
+            values = values.concat(this.inOrder(node.right));
+        }
+        return values;
+    }
+
+    // Post-Order DFS
+    postOrder(node: TreeNode<T> | null = this.root): T[] {
+        let values: T[] = [];
+    if (node) {
+        values = values.concat(this.postOrder(node.left));
+        values = values.concat(this.postOrder(node.right));
+        values.push(node.value);
+    }
+    return values;
+    }
+
 
 }
 
-function prettyPrint(node: TreeNode<number>, prefix = "", isLeft = true){
-    if (node.right !== null){
+
+
+
+function prettyPrint(node: TreeNode<number>, prefix = "", isLeft = true) {
+    if (node.right !== null) {
         prettyPrint(node.right, `${prefix}${isLeft ? "|  " : "   "}`, false);
     }
     console.log(`${prefix}${isLeft ? "└──" : "┌──"}${node.value}`);
-    
-    if(node.left !== null){
-        prettyPrint(node.left, `${prefix}${isLeft ? "|   ":"   "}`, true);
+
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "|   " : "   "}`, true);
 
     }
 }
@@ -41,9 +147,27 @@ function prettyPrint(node: TreeNode<number>, prefix = "", isLeft = true){
 const data = [5, 10, 21, 87, 301, 350];
 const tree = new Tree();
 tree.buildTree(data);
-//console.log(tree);
+// console.log(tree);
 
-prettyPrint(tree.root as TreeNode<number>)
+// tree.insert(12);
+// tree.insert(10);
+// tree.insert(2);
+// tree.insert(9);
+// tree.insert(1);
+// tree.insert(5);
+// tree.insert(11);
+// tree.insert(6);
+// tree.insert(8);
+
+//tree.recursivMethod(12);
+console.log("breadthfirst: ", tree.breadthFirst());
+console.log("preorder: ", tree.preOrder());
+console.log("inorder: ", tree.inOrder());
+console.log("postorder: ", tree.postOrder());
+
+
+
+//prettyPrint(tree.root as TreeNode<number>)
 
 
 
